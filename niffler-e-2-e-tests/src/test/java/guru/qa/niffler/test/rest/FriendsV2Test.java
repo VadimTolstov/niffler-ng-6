642ee6947c9d844d7bd2a2683719daea7895a439
VadimTolstov <tolstov50@mail.ru>
2024-12-27 22:18:41 +0300

    lesson11.3_hw19

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/rest/FriendsV2Test.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/rest/FriendsV2Test.java
@@ -5,8 +5,10 @@ import guru.qa.niffler.jupiter.annotation.Token;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.RestTest;
 import guru.qa.niffler.jupiter.extension.ApiLoginExtension;
+import guru.qa.niffler.model.rest.FriendJson;
 import guru.qa.niffler.model.rest.FriendState;
 import guru.qa.niffler.model.rest.UserJson;
+import guru.qa.niffler.service.impl.GatewayApiClient;
 import guru.qa.niffler.service.impl.GatewayApiV2Client;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,11 +24,12 @@ public class FriendsV2Test {
     @RegisterExtension
     private static final ApiLoginExtension apiLoginExtension = ApiLoginExtension.rest();
     private final GatewayApiV2Client gatewayApiV2Client = new GatewayApiV2Client();
+    private final GatewayApiClient gatewayApiClient = new GatewayApiClient();
 
     @User(friends = 2, income = 1)
     @ApiLogin
     @Test
-    void allFre(UserJson user, @Token String token) {
+    void allFriendsAndIncomeInvitationsShouldBeReturnedForUser(UserJson user, @Token String token) {
 
         final List<UserJson> expectedFriends = user.testData().friends()
                 .stream()
@@ -83,4 +86,111 @@ public class FriendsV2Test {
                 secondUserFromRequest.username()
         );
     }
+
+
+    @User(friends = 1)
+    @ApiLogin
+    @Test
+    void friendShouldBeDeleted(@Token String token) {
+        final PageImpl<UserJson> friendsBefore = gatewayApiV2Client.allFriends(
+                token,
+                null,
+                null,
+                null,
+                null
+        );
+
+        Assertions.assertEquals(1, friendsBefore.getContent().size());
+        final List<UserJson> friendToDelete = friendsBefore.stream().toList();
+
+        gatewayApiClient.removeFriend(token, friendToDelete.get(0).username());
+
+        final PageImpl<UserJson> friendsAfter = gatewayApiV2Client.allFriends(
+                token,
+                null,
+                null,
+                null,
+                null
+        );
+        Assertions.assertTrue(friendsAfter.isEmpty());
+    }
+
+    @User(income = 1)
+    @ApiLogin
+    @Test
+    void invitationShouldBeAccepted(@Token String token) {
+        final PageImpl<UserJson> invitationsBefore = gatewayApiV2Client.allFriends(
+                token,
+                null,
+                null,
+                null,
+                null
+        );
+
+        Assertions.assertEquals(1, invitationsBefore.getContent().size());
+        final List<UserJson> invitation = invitationsBefore.stream().toList();
+
+        gatewayApiClient.acceptInvitation(token, new FriendJson(invitation.get(0).username()));
+
+        final PageImpl<UserJson> friendsAfter = gatewayApiV2Client.allFriends(
+                token,
+                null,
+                null,
+                null,
+                null
+        );
+        Assertions.assertEquals(1, friendsAfter.getContent().size());
+        final List<UserJson> friends = friendsAfter.stream().toList();
+        Assertions.assertEquals(FriendState.FRIEND, friends.getFirst().friendState());
+    }
+
+    @User(income = 1)
+    @ApiLogin
+    @Test
+    void invitationShouldBeDeclined(@Token String token) {
+        final PageImpl<UserJson> invitationsBefore = gatewayApiV2Client.allFriends(
+                token,
+                null,
+                null,
+                null,
+                null
+        );
+        Assertions.assertEquals(1, invitationsBefore.getContent().size());
+        final List<UserJson> invitation = invitationsBefore.stream().toList();
+
+        gatewayApiClient.declineInvitation(token, new FriendJson(invitation.get(0).username()));
+
+        final PageImpl<UserJson> invitationsAfter = gatewayApiV2Client.allFriends(
+                token,
+                null,
+                null,
+                null,
+                null
+        );
+        Assertions.assertTrue(invitationsAfter.isEmpty());
+    }
+
+    @User(outcome = 1)
+    @ApiLogin
+    @Test
+    void outgoingInvitationShouldBeCreated(UserJson user, @Token String token) {
+        final String outcomeInvitationUsername = user.testData().outcomeInvitationsUsernames()[0];
+        final FriendJson newFriend = new FriendJson(outcomeInvitationUsername);
+
+        gatewayApiClient.sendInvitation(token, newFriend);
+
+        final List<UserJson> outgoingInvitations = gatewayApiV2Client.allUsers(
+                        token,
+                        null,
+                        null,
+                        null,
+                        null
+                ).stream()
+                .filter(u -> u.friendState() == FriendState.INVITE_SENT)
+                .toList();
+
+        Assertions.assertEquals(1, outgoingInvitations.size());
+        Assertions.assertEquals(newFriend.username(), outgoingInvitations.getFirst().username());
+    }
 }
+
